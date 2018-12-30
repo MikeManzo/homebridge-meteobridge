ba96198fc692f373ee03d9b4e74f537b0c171d66
Mike Manzo <mike@dragon.rusticcoders.com>
2018-12-29 22:56:28 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# homebridge-meteboridge
-Homebridge plugin for getting weather measurements straight from a connected Meteobridge device
+# homebridge-meteobridge
+A pretty basic plugin for Homebridge to display Temperature and Humidity for [meteobridge-](https://www.meteobridge.com/wiki/index.php/Home)connected weatherstations.  Currenlty the plugin relies on your locally-connected meteobridge to relay and display weather station data (temperature and humidity) to the Home App.
+
+**Special Note:** As with most things in HomeKit, you can ask Siri on your iOS (or HomePod) device to read out the temp and/or humidity based on how you setup your devices in the Home App.
+
+## Quick Installation
+
+1. Install homebridge using: **_npm install -g homebridge_**
+2. Install this plugin using: **_npm install -g homebridge-meteobridge_**
+3. Update your configuration file to reflect your local conditions (**see the sample below**)
+
+No keys or APIs required.  We just rely on the fact that **(a)** you have a weather station on your local networtk connected through a Meteobridge and **(b)** Meteobridge exposes templates for us to take advantage of.  
+
+## Meteobridge References
+1. [Meteobridge Templates](https://www.meteobridge.com/wiki/index.php/Templates)
+2. [Meteobridge Add-On Services](https://www.meteobridge.com/wiki/index.php/Add-On_Services)
+
+
+## Sample Configuration
+Add the following information to your config.json file (without the <-- comments)
+```
+
+   "accessories": [
+        {
+            "accessory": "HomeMeteobridge",
+            "name": "Meteobridge",
+            "ip_address":  "A.B.C.D",
+            "frequency": 5000,
+            "password": "<password>",
+            "debug": "true"
+        }
+    ]
+```
+### Field Explainations
+           "accessory": "HomeMeteobridge",      <-- Our Accessory name
+            "name": "Meteobridge",              <-- Easy to remember name
+            "ip_address":  "A.B.C.D",           <-- IP address of our meteobridge
+            "frequency": 5000,                  <-- Update frequency (5 second min)
+            "password": "<password>",           <-- Admin password for Meteobridge
+            "debug": "true"                     <-- Display temp/humidity readings in the console
+
+#### Things yet to do:
+1. Add support for other sensors like wind, rain, etc ...
+2. Add support for forecasts ...
+3. ??  Any ideas ??
+
+#### Credits
+This plugin was heavily influenced by the Thorsten Voß's [homebridge-wunderground](https://github.com/xfjx/homebridge-wunderground) work.  With Wunderground likely becoming inaccessible as a free service in teh not-so-distant future, I chose to make this "local" version for people to use (if they have a Meteobridge-connected weather station, of course).
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+/*
+Copyright 2018, Mike Manzo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 var request = require("request");
 var Service, Characteristic;
 
+var temperatureService;
+var humidityService;
+
+/*
+Very important to export and expose our name and accessories!
+*/
 module.exports = function(homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
@@ -8,102 +28,164 @@ module.exports = function(homebridge) {
     homebridge.registerAccessory("homebridge-meteobridge", "HomeMeteobridge", HomeMeteobridgeAccessory);
 };
 
+/*
+Set up the accessory with the IP of the Meteobridge, the admin password, and the frequency you wish to update the sensors.
+Currently the limit is set to once every 5 seconds.
+
+We are going to be looking for the following:
+    "accessories": [
+        {
+            "accessory": "HomeMeteobridge",     <-- Our Accessory name
+            "name": "Meteobridge",              <-- Easy to remember name
+            "ip_address":  "A.B.C.D",           <-- IP address of our meteobridge
+            "frequency": 5000,                  <-- Update frequency (5 second min)
+            "password": "<password>",           <-- Admin password for Meteobridge
+            "debug": "true"                     <-- Display temp/humidity readings in teh console
+        }
+    ]
+*/
 function HomeMeteobridgeAccessory(log, config) {
-    this.log = log;
-    this.name = config["name"];
-    this.url = config["url"];
-    this.type = config["type"] || "page";
-    this.json_url = config["json_url"] || null;
-    this.temp_url = config["temp"];
-    this.humi_url = config["humidity"];
-    this.wind_url = config["wind"];
-    this.freq = config["freq"] || 1000;
+    this.weather    = {};
+    this.log        = log;
+    this.name       = config["name"];
+    this.ip         = config["ip_address"];
+    this.freq       = config["frequency"];
+    this.passoword  = config["password"];
+    this.debug      = config["debug"]
 
-    this.temperature = 0;
-    this.humidity = 0;
-    this.wind = 0;
+    if (this.freq < 5000) {
+        this.log.info("Warning: Minimum polling of Meteobridge is currently set to 5 seconds.");
+        freq = 5000;
+    }
 
-    this.services = [];
+    this.weather.temperature = 0.0
+    this.weather.humidity    = 0.0
 
-    // Temperature
-    this.temperatureService = new Service.TemperatureSensor ("Temperature Sensor");
-    this.temperatureService
-    .getCharacteristic(Characteristic.CurrentTemperature)
-    .on('get', this.getValue.bind(this, 'temperature'));
-    this.services.push(this.temperatureService);    
-    
-    // Humidity
-    this.humidityService = new Service.HumiditySensor ("Humidity Sensor");
-    this.humidityService
-    .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-    .on('get', this.getValue.bind(this, 'humidity'));
-    this.services.push(this.humidityService);
-
-    // Wind
-    this.windService = new Service.WindSensor  ("Wind Sensor");
-    this.windService
-    .getCharacteristic(Characteristic.CurrentWindSpeed)
-    .on('get', this.getValue.bind(this, 'wind'));
-    this.services.push(this.windService);
-
-    setInterval(() => {
-        this.getValue(null, (err, { humidity, temperature, wind}) => {
-
-        this.temperatureService
-        .setCharacteristic(Characteristic.CurrentTemperature, temperature);
-
-        this.humidityService
-        .setCharacteristic(Characteristic.CurrentRelativeHumidity, humidity);
-
-        this.windService
-        .setCharacteristic(Characteristic.CurrentWindSpeed, wind);
-        });}, this.freq);
+    var that = this;
+    setTimeout(function() {
+        that.servicePolling();
+    }, this.freq);
 }
 
-HomeMeteobridgeAccessory.prototype.getValue = function(name, callback) {
-    if(type == "page"){
-        // http://10.0.0.137/cgi-bin/template.cgi?template=[th0temp-act=F.1:---]
-        request(this.url + "/cgi-bin/template.cgi?template=[th0temp-act=F.1:---]", (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var temperature = parseInt(body, 10);
-                if(name == "temperature"){
-                    return callback(null, temperature);
-                }
-                else{ // http://10.0.0.137/cgi-bin/template.cgi?template=[th0hum-act:---]
-                    request(this.url + "/cgi-bin/template.cgi?template=[th0hum-act:---]", (error, response, body) => {
-                        if (!error && response.statusCode == 200) {
-                            var humidity = parseInt(body, 10);
-                            if(name == "humidity"){
-                                return callback(null, humidity);
-                            }
-                            else{ // http://10.0.0.137/cgi-bin/template.cgi?template=[wind0wind-act:---]
-                                request(this.url + "/cgi-bin/template.cgi?template=[wind0wind-act:---]", (error, response, body) => {
-                                    if (!error && response.statusCode == 200) {
-                                        var light = parseInt(body, 10);
-                                        if(name == "wind"){
-                                            return callback(null, light);
-                                        }
-                                        else{
-                                            return callback(null, { humidity: humidity, temperature: temperature, light: light });
-                                        } //End: else, name != "humidity"
-                                    } //End: if OK respone
-                                }); //End: request light
-                            } //End: else, name != "humidity"
-                        } //End: if OK respone
-                    }); //End: request humidity
-                } //End: else, name != "temperature"
-            } //End: if OK respone
-        }); //End: request temperature
-    } else {
-        request(this.url + this.json_url, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var obj = JSON.parse(body);
-                return callback(null, { humidity: obj.humidity, temperature: obj.temperature, wind: obj.wind });
-            } 
-        });
-    }
-};
+/*
+servicePolling: This is the callback function for the getState method
+*/
+HomeMeteobridgeAccessory.prototype = {
+    servicePolling: function() {      
+		this.getState(function(measurement) {
+			var that = this;
+        
+            if (that.debug == 'true') {
+                that.log.info("Weatherstation Temperature is: %s, Humidity is: %s", measurement.temperature, measurement.humidity);
+            }
 
-HomeMeteoAccessory.prototype.getServices = function() {
-    return this.services;
+			that.temperatureService.setCharacteristic(Characteristic.CurrentTemperature, measurement.temperature || 0);
+			that.humidityService.setCharacteristic(Characteristic.CurrentRelativeHumidity, measurement.humidity || 0);
+        
+            setTimeout(function() {
+				that.servicePolling();
+            }, that.freq);
+            
+		}.bind(this));
+	},
+
+    /*
+    What is the state of the humidity accessory?
+    */
+    getStateHumidity: function(callback){
+        this.getState(function(w) {
+            callback(null, w.humidity || 0);
+        });
+    },
+
+    /*
+    What is the state of the temperature accessory?
+    */
+   getStateTemperature: function(callback){
+        this.getState(function(w) {
+            callback(null, w.temperature || 0);
+        });
+    },
+
+    /*
+    The heart of the matter.  Refer to the following for template generation for temp and humidy:
+        1. https://www.meteobridge.com/wiki/index.php/Templates
+        2. https://www.meteobridge.com/wiki/index.php/Add-On_Services
+
+    We will need to build the appropriate request for our data: 
+        1. http://your.unique.station.ip/cgi-bin/template.cgi?template=[th0temp-act=.1:---],[th0hum-act:---]
+
+    Once we check for validity, we simply convert our data and format our weather object accordingly .. then call the callback.
+    NOTE: We will need to pass the user credentials for your meteobridge with your request.  Remember:
+        username: meteobridge
+        password: <default> meteobridge <-- You had better have changed THIS!
+    */
+    getState: function (callback) {
+        var that = this
+
+        request("http://" + "meteobridge:" + this.passoword + "@" + that.ip + "/cgi-bin/template.cgi?template=[th0temp-act=.1:---],[th0hum-act:---]", (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                var data = body.split(',');
+                if (!isNaN(data[0]) && !isNaN(data[1])) {
+                    that.weather.temperature    = parseFloat(data[0]);
+                    that.weather.humidity       = parseFloat(data[1]);
+                    that.temperatureService.setCharacteristic(Characteristic.StatusFault, 0);
+                    that.humidityService.setCharacteristic(Characteristic.StatusFault, 0);
+                } else {
+                    that.weather.temperature    = 0;
+                    that.weather.humidity       = 0;
+                    that.temperatureService.setCharacteristic(Characteristic.StatusFault, 1);
+                    that.humidityService.setCharacteristic(Characteristic.StatusFault, 1);
+                }
+            } else {
+                that.log.info("Error retrieving temperature & humidity data from: " + that.ip);
+                that.temperatureService.setCharacteristic(Characteristic.StatusFault, 1);
+                that.humidityService.setCharacteristic(Characteristic.StatusFault, 1);
+            }
+        });
+
+        callback(that.weather);
+    },
+
+    identify: function (callback) {
+        this.log("Identify requested!");
+        callback(); // success
+    },
+
+    /*
+    Set up the services we are going to expose.  Very simple ... Temperature and humidity for us.
+    */
+    getServices: function () {
+        var services = []
+        var informationService = new Service.AccessoryInformation();
+
+        informationService
+            .setCharacteristic(Characteristic.Manufacturer, "HomeBridge")
+            .setCharacteristic(Characteristic.Model, "Meteobridge")
+  		services.push(informationService);
+
+        this.temperatureService = new Service.TemperatureSensor(this.name + " Temperature"); // Sensor will display "Meteobridge Temperature" 
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .on('get', this.getStateTemperature.bind(this));
+
+        // Let's set a minimum temperature of -20
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({minValue: -20});
+
+        // Let's set a maximum temperature of 120
+        this.temperatureService
+            .getCharacteristic(Characteristic.CurrentTemperature)
+            .setProps({maxValue: 120});
+		services.push(this.temperatureService);
+
+        this.humidityService = new Service.HumiditySensor(this.name + " Humidity"); // Sensor will display "Meteobridge Humidity"
+        this.humidityService
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .on('get', this.getStateHumidity.bind(this));
+		services.push(this.humidityService);
+
+        return services;
+    }
 };
