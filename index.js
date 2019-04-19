8b53b729921d73ee7d36f3cf8eaa4e733ced9a52
Mike Manzo <manzo.mike@gmail.com>
2019-04-19 16:42:13 -0400

    Revert "Initial Commit"
    This reverts commit e2ca654bdc1ba79d2f1984ce04f5df75ff38302f.

--- index.js
+++ index.js
@@ -12,12 +12,11 @@
     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-var request = require("request");               // Async HTTP Requests
-var syncRequest = require('sync-request');      // Sync HTTP Requests
-var rp = require('request-promise');            // Promises Support
-var inherits = require('util').inherits;        // Inheritance Support
-const moment = require('moment');               // Unix Tiime
-var xmlParser = require('xml2json');            // XML to JSON Object
+var request = require("request");           // Async HTTP Requests
+var syncRequest = require('sync-request');  // Sync HTTP Requests
+var rp = require('request-promise');        // Promises Support
+var inherits = require('util').inherits;    // Inheritance Support
+const moment = require('moment');           // Unix Tiime
 
 var Service, Characteristic, FakeGatoHistoryService;
 
@@ -34,50 +33,42 @@ module.exports = function(homebridge) {
     FakeGatoHistoryService = require('fakegato-history')(homebridge);
 
     // Register the whole thing ...
-    homebridge.registerAccessory("homebridge-meteohub", "HomeMeteohub", HomeMeteohubAccessory);
+    homebridge.registerAccessory("homebridge-meteobridge", "HomeMeteobridge", HomeMeteobridgeAccessory);
 };
 
 /*
-    Set up the accessory with the IP OR Server Address of the Meteohub, the admin password, and the frequency you wish to update the sensors.
+    Set up the accessory with the IP of the Meteobridge, the admin password, and the frequency you wish to update the sensors.
     Currently the limit is set to once every 5 seconds.
 
     We are going to be looking for the following:
         "accessories": [
             {
-                "accessory": "HomeMeteohub",            <-- Our Accessory name
-                "name": "Meteohub",                     <-- Easy to remember name
-                "ip_address":  "A.B.C.D",               <-- IP address of our meteohub  <pick IP OR server; leave the other empty --> "">
-                "server_address: "your.domain.name"     <-- MeteoHub server FQ address  <pick server OR IP; leave the other empty --> "">
-                "port": "1234"                          <-- Desired Port
-                "frequency": 5000,                      <-- Update frequency (5 second min)
-                "password": "<password>",               <-- Admin password for Meteohub <Currently NOT used>
-                "debug": "true"                         <-- Display temp/humidity readings in the console
+                "accessory": "HomeMeteobridge",     <-- Our Accessory name
+                "name": "Meteobridge",              <-- Easy to remember name
+                "ip_address":  "A.B.C.D",           <-- IP address of our meteobridge
+                "frequency": 5000,                  <-- Update frequency (5 second min)
+                "password": "<password>",           <-- Admin password for Meteobridge
+                "debug": "true"                     <-- Display temp/humidity readings in teh console
             }
         ]
     
     For the Eve app, we need a couple of things:
-        1. this.displayName <-- Without this, the logging fails silently (start homebeidge in DEBUG mode to look for 'undefined' acccessory tagging)
+        1. this.displayName <-- Without this, the loggins fails silently (start homebeidge in DEBUG mode to look for 'undefined' acccessory tagging)
         2. A simple callback to post the data to FakeGato
 */
-function HomeMeteohubAccessory(log, config) {
+function HomeMeteobridgeAccessory(log, config) {
     this.weather        = {};
     this.log            = log;
     this.name           = config["name"];
     this.ip             = config["ip_address"];
     this.freq           = config["frequency"];
     this.passoword      = config["password"];
-    this.port           = config["port"]
-    this.server         = config["server_address"]
     this.debug          = config["debug"]
     this.displayName    = config["name"];
 
     if (this.freq < 5000) {
-        this.log.info("Warning: Minimum polling of Meteohub is currently set to 5 seconds.");
+        this.log.info("Warning: Minimum polling of Meteobridge is currently set to 5 seconds.");
         freq = 5000;
-    }
-
-    if (this.ip.length > 0 && this.server.length > 0) {
-        this.log.error("Error: Both IP address and Server address defined.  Only define one.  Defaulting to IP Address")
     }
 
     this.weather.temperature    = 0.0;
@@ -95,7 +86,7 @@ function HomeMeteohubAccessory(log, config) {
 /*
     servicePolling: This is the callback function for the getState method
 */
-HomeMeteohubAccessory.prototype = {
+HomeMeteobridgeAccessory.prototype = {
     servicePolling: function() {      
 		this.getState(function(measurement) {
 			var that = this;
@@ -216,108 +207,63 @@ HomeMeteohubAccessory.prototype = {
     },
 
     /*
+        The heart of the matter.  Refer to the following for template generation for temp and humidy:
+            1. https://www.meteobridge.com/wiki/index.php/Templates
+            2. https://www.meteobridge.com/wiki/index.php/Add-On_Services
+
         We will need to build the appropriate request for our data: 
-            1. http://your.domain.name:your_port/meteolog.cgi?type=xml&quotes=1&mode=data&info=station&info=utcdate&info=sensorids
+            1. http://your.unique.station.ip/cgi-bin/template.cgi?template=[th0temp-act=.1:---],[th0hum-act:---]
             2. Data is returned as follows:
-                <logger>
-                <TH date="20190419143347" id="th0" temp="18.4" hum="67" dew="12.2" lowbat="0" />
-                <WIND date="20190419143355" id="wind0" dir="65" gust="11.2" wind="8.0" chill="17.2" lowbat="0" />
-                <RAIN date="20190419143305" id="rain0" rate="0.0" total="58.4" delta="0.0" lowbat="0" />
-                <THB date="20190419143335" id="thb0" temp="23.1" hum="74" dew="18.2" press="1015.5" seapress="1017.4" fc="2" lowbat="1" />
-                </logger>
-            3. We then convert to JSON
-                { 
-                    logger: { 
-                        TH: { 
-                            date: '20190419183611',
-                            id: 'th0',
-                            temp: '17.5',
-                            hum: '69',
-                            dew: '11.7',
-                            lowbat: '0' 
-                        },
-                    WIND: { 
-                        date: '20190419183615',
-                        id: 'wind0',
-                        dir: '68',
-                        gust: '4.0',
-                        wind: '3.6',
-                        chill: '17.2',
-                        lowbat: '0' 
-                    },
-                    RAIN: { 
-                        date: '20190419183525',
-                        id: 'rain0',
-                        rate: '0.0',
-                        total: '58.4',
-                        delta: '0.0',
-                        lowbat: '0'
-                    },
-                    THB: { 
-                        date: '20190419183537',
-                        id: 'thb0',
-                        temp: '23.3',
-                        hum: '74',
-                        dew: '18.4',
-                        press: '1017.1',
-                        seapress: '1019.0',
-                        fc: '3',
-                        lowbat: '1'
-                    } 
-                }
-            }
+                data[0]: Temperature
+                data[1]: Humidity
+                data[2]: Pressure
+                data[3]: UV
+                data[4]: Wind Speed
+                data[5]: Battery Health (1 is low battery)
 
         Once we check for validity, we simply convert our data and format our weather object accordingly .. then call the callback.
-        NOTE: We will need to pass the user credentials for your meteohub with your request.  Remember:
-            username: meteohub
-            password: <default> meteohub <-- You had better have changed THIS!
+        NOTE: We will need to pass the user credentials for your meteobridge with your request.  Remember:
+            username: meteobridge
+            password: <default> meteobridge <-- You had better have changed THIS!
 
         Fault Codes:
             0 - No Fault
-            1 - meteohub returned NaN (Not a Number) ... retry
-            2 - meteohub is just not responding; check the comsole log for the error and statusCode 
+            1 - Meteobridge returned NaN (Not a Number) ... retry
+            2 - Meteobridge is just not responding; check the comsole log for the error and statusCode 
     */
     getState: function (callback) {
         var that = this
-        var address = ""
 
-        if (that.ip.length > 0 && that.server.length > 0) {
-            address = that.ip
-        } else if (that.ip.length > 0) {
-            address = that.ip
-        } else {
-            address = that.server
-        }
-
-        request("http://" + address + ":" + that.port + "/meteolog.cgi?type=xml&quotes=1&mode=data&info=station&info=utcdate&info=sensorids", (error, response, body) => {
+        request("http://" + "meteobridge:" + this.passoword + "@" + that.ip + "/cgi-bin/template.cgi?template=[th0temp-act=.1:---],[th0hum-act:---],[thb0press-act:---],[uv0index-act:---],[wind0wind-act:---],[th0lowbat-act:---]",
+                                                                                (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var options = {
-                    object: true,
-                    reversible: false,
-                    coerce: false,
-                    sanitize: true,
-                    trim: true,
-                    arrayNotation: false,
-                    alternateTextNode: false
-                };
+                var data = body.split(',');
+                if (!isNaN(data[0]) && !isNaN(data[1])) {
+                    that.weather.temperature    = parseFloat(data[0]);  // Temp
+                    that.weather.humidity       = parseFloat(data[1]);  // Humidity
+                    that.weather.airpresssure   = parseFloat(data[2]);  // Pressure
+                    that.weather.uv             = parseFloat(data[3]);  // Ultra Violet Index
+                    that.weather.windspeed      = parseFloat(data[4]);  // Wind Speed
 
-                var json = xmlParser.toJson(body, options);
 
-                that.weather.temperature    = json.logger.TH.temp;      // Temp
-                that.weather.humidity       = json.logger.TH.hum;       // Humidity
-                that.weather.airpresssure   = json.logger.THB.press;    // Pressure
-                that.weather.uv             = 0                         // Ultra Violet Index
-                that.weather.windspeed      = json.logger.WIND.wind;    // Wind Speed
+                    that.humidityService.setCharacteristic(Characteristic.StatusActive, 1);
+                    that.humidityService.setCharacteristic(Characteristic.StatusLowBattery, parseInt(data[5]));
+                    that.humidityService.setCharacteristic(Characteristic.StatusFault, 0);
 
-                that.humidityService.setCharacteristic(Characteristic.StatusActive, 1);
-                that.humidityService.setCharacteristic(Characteristic.StatusLowBattery, json.logger.TH.lowbat);
-                that.humidityService.setCharacteristic(Characteristic.StatusFault, 0);
+                    that.temperatureService.setCharacteristic(Characteristic.StatusActive, 1);
+                    that.temperatureService.setCharacteristic(Characteristic.StatusLowBattery, parseInt(data[5]));
+                    that.temperatureService.setCharacteristic(Characteristic.StatusFault, 0);
+                } else {
+                    that.log.info("Did not recieve valid data from the Meteobridge; keeping previous values & faulting.  Next attempt in " + this.freq/1000 + " seconds.");
 
-                that.temperatureService.setCharacteristic(Characteristic.StatusActive, 1);
-                that.temperatureService.setCharacteristic(Characteristic.StatusLowBattery, json.logger.TH.lowbat);
-                that.temperatureService.setCharacteristic(Characteristic.StatusFault, 0);
+                    that.temperatureService.setCharacteristic(Characteristic.StatusFault, 1);
+                    that.temperatureService.setCharacteristic(Characteristic.StatusActive, 0);
+
+                    that.humidityService.setCharacteristic(Characteristic.StatusFault, 1);
+                    that.humidityService.setCharacteristic(Characteristic.StatusActive, 0);
+                }
             } else {
-                that.log.error("Error retrieving hub data from: " + address + " because of error: " + error + " with a response code of: " + response.statusCode);
+                that.log.error("Error retrieving station data from: " + that.ip + " because of error: " + error + " with a response code of: " + response.statusCode);
                 that.temperatureService.setCharacteristic(Characteristic.StatusFault, 2);
                 that.temperatureService.setCharacteristic(Characteristic.StatusActive, 0);
 
@@ -336,11 +282,11 @@ HomeMeteohubAccessory.prototype = {
     /*
         Set up the services we are going to expose.  Very simple ... Temperature and humidity for us.
             Note (1):   I chose to use "sync-request" because I wanted the getServices fucntion to block until we recieved thespecificzations
-                        from the Meteohub.  I could not figure out a way to do it in the completion block of the async request call (I even tried promises).
+                        from the Meteobridge.  I could not figure out a way to do it in the completion block of the async request call (I even tried promises).
                         If someone can show me a way to accomplish this w/o a bnlocing call, I will use it.  For now, this function gets called once.  We can 
                         block until it returns.
             Note (2):   Here is the promise block I would have liked to have used
-                            rp("http://" + "meteohub:" + this.passoword + "@" + this.ip + "/cgi-bin/template.cgi?template=[mbsystem-mac],[mbsystem-platform],[mbsystem-swversion]")
+                            rp("http://" + "meteobridge:" + this.passoword + "@" + this.ip + "/cgi-bin/template.cgi?template=[mbsystem-mac],[mbsystem-platform],[mbsystem-swversion]")
                                 .then(function (result) {
                                     var data = result.split(',');
                                     serialNum = data[0];
@@ -360,40 +306,24 @@ HomeMeteohubAccessory.prototype = {
         var serialNum = "Unknown"
         var model = "Unknown"
         var swVersion = 0.0
-        var address = ""
 
         var informationService = new Service.AccessoryInformation();
         this.services = [];
         var that = this;
 
-        if (that.ip.length > 0 && that.server.length > 0) {
-            address = that.ip
-        } else if (that.ip.length > 0) {
-            address = that.ip
-        } else {
-            address = that.server
-        }
-
-        var myURL = "http://" + address + ":" + that.port + "/meteograph.cgi?text=actual_system_platform_text";
+        var myURL = "http://" + "meteobridge:" + this.passoword + "@" + this.ip + "/cgi-bin/template.cgi?template=[mbsystem-mac],[mbsystem-platform],[mbsystem-swversion]";
         var result = syncRequest('GET', myURL);
         if (result.statusCode == 200) {
             var body = result.getBody().toString();
-            model = body
+            var data = body.split(',');
+            serialNum = data[0];
+            model = data[1];
+            swVersion = data[2];
         } else {
             that.log.error("*** Error ***: " + result.statusCode);
-            that.log.info("*** Warning ***: Unable to determine Meteohub platform; defaults are being used.");
+            that.log.info("*** Warning ***: Unable to determine Meteobridge specifics; defaults are being used.");
         }
     
-        myURL = "http://" + address + ":" + that.port + "/meteograph.cgi?text=actual_system_version_text";
-        var result = syncRequest('GET', myURL);
-        if (result.statusCode == 200) {
-            var body = result.getBody().toString();
-            swVersion = body;
-        } else {
-            that.log.error("*** Error ***: " + result.statusCode);
-            that.log.info("*** Warning ***: Unable to determine Meteohub software version; defaults are being used.");
-        }
-
         // Custom Airpressure Characteristic
         AirPressure = function () {
             Characteristic.call(this, 'Air Pressure', '7e4d6810-5dd3-45ea-a24f-7190f883e2f6');
@@ -475,8 +405,8 @@ HomeMeteohubAccessory.prototype = {
             .setCharacteristic(Characteristic.Manufacturer, "smartbedded GmbH")
             .setCharacteristic(Characteristic.Model, "Platform Type: " + model)
             .setCharacteristic(Characteristic.SerialNumber, serialNum)
-            .setCharacteristic(Characteristic.FirmwareRevision, "Meteohub: " + swVersion)
-            .setCharacteristic(Characteristic.Name, "Meteohub")
+            .setCharacteristic(Characteristic.FirmwareRevision, "Meteobridge: " + swVersion)
+            .setCharacteristic(Characteristic.Name, "Meteobridge")
         
         informationService.addCharacteristic(Characteristic.StatusActive);
         informationService.addCharacteristic(Characteristic.StatusFault);
